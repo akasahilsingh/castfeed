fe5692138cb3c86350e259f8c3ccedf2eb96a475
Sahil Singh <akasahilsingh@gmail.com>
2026-09-18 14:26:46 +0530

    feat: enhance getVideoById function to include likes, subscriptions, and improved video retrieval logic

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -262,8 +262,7 @@ const publishAVideo = asyncHandler(async (req, res) => {
 
 const getVideoById = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
-  //TODO: get video by id
-  // Further improvement The user should get likes total and comments and if you have liked or not and etc and also subscribers.
+  // Further improvement comments
   if (!videoId) {
     throw new ApiError(400, "Video is required");
   }
@@ -271,9 +270,100 @@ const getVideoById = asyncHandler(async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(videoId)) {
     throw new ApiError(400, "Not a valid video Id");
   }
-  const video = await Video.findById(videoId);
+  // const video = await Video.findById(videoId);
 
-  if (!video) {
+  const video = await Video.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $lookup: {
+              from: "subscriptions",
+              localField: "_id",
+              foreignField: "channel",
+              as: "subscribers",
+            },
+          },
+          {
+            $addFields: {
+              subscribersCount: {
+                $size: "$subscribers",
+              },
+              isSubscribed: {
+                $cond: {
+                  if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+                  then: true,
+                  else: false,
+                },
+              },
+            },
+          },
+          {
+            $project: {
+              userName: 1,
+              avatar: 1,
+              subscribersCount: 1,
+              isSubscribed: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        likesCount: {
+          $size: "$likes",
+        },
+        owner: {
+          $first: "$owner",
+        },
+        isLiked: {
+          $cond: {
+            if: { $in: [req.user?._id, "$likes.likedby"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        videoFile: 1,
+        thumbnail: 1,
+        title: 1,
+        description: 1,
+        duration: 1,
+        views: 1,
+        createdAt: 1,
+        userName: 1,
+        avatar: 1,
+        subscribersCount: 1,
+        isSubscribed: 1,
+        likesCount: 1,
+        owner: 1,
+        isLiked: 1,
+      },
+    },
+  ]);
+
+  if (!video.length) {
     throw new ApiError(404, "Video not found");
   }
 
@@ -291,7 +381,7 @@ const getVideoById = asyncHandler(async (req, res) => {
 
   return res
     .status(200)
-    .json(new ApiResponse(200, video, "Video fetched successfully"));
+    .json(new ApiResponse(200, video[0], "Video fetched successfully"));
 });
 
 const updateVideo = asyncHandler(async (req, res) => {
